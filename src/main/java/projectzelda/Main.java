3288db44a2c6adb551e9d9d66d0d646d637e1363
Micah D <micah.git@dvyld.com>
2021-05-23 17:48:59 +0200

    Read in xml

--- src/main/java/projectzelda/Main.java
+++ src/main/java/projectzelda/Main.java
@@ -4,6 +4,13 @@ import projectzelda.engine.*;
 import projectzelda.game.*;
 import projectzelda.gfx.*;
 
+import java.io.File;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+
 // (c) Thorsten Hasbargen
 
 final class Main 
@@ -29,6 +36,21 @@ final class Main
     }
 
     public static void main(String[] args) {
-        new Main();
+        try {
+            File file = new File("world_map.tmx");
+            DocumentBuilderFactory documentBuilderFactory = 
+                DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            Document document = documentBuilder.parse(file);
+            NodeList layers = document.getElementsByTagName("layer");
+            for (int i = 0; i < layers.getLength(); i++) {
+                Node layer = layers.item(i);
+                System.out.println(layer + layer.getTextContent());
+            }
+            //String pwd = document.getElementsByTagName("password").item(0).getTextContent();
+            new Main();
+        } catch(Exception e) {
+            System.out.println("Exception! " + e.getMessage());
+        }
     }
 }
