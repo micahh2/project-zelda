dc3555ecc58f121c666b0424993574219288f249
Micah D <micah.git@dvyld.com>
2021-05-24 13:24:49 +0200

    Read initial tmx data

--- src/main/java/projectzelda/Main.java
+++ src/main/java/projectzelda/Main.java
@@ -3,13 +3,8 @@ package projectzelda;
 import projectzelda.engine.*;
 import projectzelda.game.*;
 import projectzelda.gfx.*;
+import projectzelda.map.*;
 
-import java.io.File;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.DocumentBuilder;
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Node;
 
 // (c) Thorsten Hasbargen
 
@@ -36,21 +31,8 @@ final class Main
     }
 
     public static void main(String[] args) {
-        try {
-            File file = new File("world_map.tmx");
-            DocumentBuilderFactory documentBuilderFactory = 
-                DocumentBuilderFactory.newInstance();
-            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-            Document document = documentBuilder.parse(file);
-            NodeList layers = document.getElementsByTagName("layer");
-            for (int i = 0; i < layers.getLength(); i++) {
-                Node layer = layers.item(i);
-                System.out.println(layer + layer.getTextContent());
-            }
-            //String pwd = document.getElementsByTagName("password").item(0).getTextContent();
-            new Main();
-        } catch(Exception e) {
-            System.out.println("Exception! " + e.getMessage());
-        }
+        Map map = new Map("world_map.tmx");
+        System.out.println(map);
+        new Main();
     }
 }
