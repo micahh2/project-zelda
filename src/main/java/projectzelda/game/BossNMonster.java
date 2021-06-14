435fe5a8bb4c9f242112c1c8fddf748b2807e70d
Micah D <micah.git@dvyld.com>
2021-06-14 16:17:11 +0200

    Baddies track the avater when nearby

--- src/main/java/projectzelda/game/BossNMonster.java
+++ src/main/java/projectzelda/game/BossNMonster.java
@@ -4,14 +4,31 @@ import projectzelda.Const;
 import projectzelda.engine.GraphicSystem;
 import projectzelda.engine.ImageRef;
 import projectzelda.engine.WorldInfo;
-import projectzelda.engine.UIObject;
+import projectzelda.engine.Point;
 
 public class BossNMonster extends GoblinAI
 {
-    public BossNMonster(double x, double y, ImageRef imageRef, WorldInfo worldInfo)
+    public Avatar avatar;
+
+    public BossNMonster(double x, double y, ImageRef imageRef, Avatar avatar)
     {
-        super(x, y, worldInfo);
+        super(x, y, null);
         this.imageRef = imageRef;
+        this.avatar = avatar;
+    }
+
+    @Override
+    public Point newDestination() {
+        return new Point<Double>(avatar.x, avatar.y);
+    }
+
+    @Override
+    public boolean readyForNewDestination() {
+        double dist = world
+                .getPhysicsSystem()
+                .distance(x, y, avatar.x, avatar.y);
+
+        return dist < 160;
     }
 
     @Override 
